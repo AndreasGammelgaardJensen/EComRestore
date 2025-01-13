1101ae72d7054de1324ecc10dffb55901ff41ac2
Andreas BG Jensen <andreas.jensen@twoday.com>
2025-01-13 22:08:20 +0100

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController(StoreContext context) : ControllerBase
+	{
+		private StoreContext _context { get; } = context;
+
+		// GET: api/<ProductController>
+		[HttpGet]
+		public async Task<ActionResult<List<Product>>> GetProducts()
+		{
+			return await _context.Products.ToListAsync();
+		}
+
+		// GET api/<ProductController>/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+
+			if(product is null) return NotFound();
+
+			return product;
+		}
+	}
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace API.Entities
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public required string Name { get; set; }
+		public required string Description { get; set; }
+		public long Price { get; set; }
+		public required string PictureUrl { get; set; }
+		public required string Type { get; set; }
+		public required string Brand { get; set; }
+		public int QuantityInStock { get; set; }
+
+	}
+}
